421626114a1008d1d85a90cc3a95755b74c67ee4
Mohammed Bachiri Jabbouri <mohammedbachiri17@gmail.com>
2025-11-02 23:03:21 +0100

    ToDo App Terminada

--- ToDoApp/app/build.gradle.kts
+++ ToDoApp/app/build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -40,7 +42,8 @@ android {
 }
 
 dependencies {
-
+    
+    implementation(libs.androidx.compose.material.icons.extended.v150)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +60,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
 }
--- ToDoApp/app/src/main/java/com/example/todoapp/MainActivity.kt
+++ ToDoApp/app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -7,26 +7,45 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.RestoreFromTrash
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat.enableEdgeToEdge
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.todoapp.ui.theme.TodoappTheme
 import org.intellij.lang.annotations.JdkConstants
 
@@ -36,15 +55,43 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodoappTheme {
-                Login()
+                TodoApp()
             }
         }
     }
 }
 
+var globalNombre = ""
+var globalAlias = ""
+
 @Composable
-fun Login() {
-    val context = LocalContext.current // 🔹 Obtenemos el contexto actual
+fun TodoApp(){
+
+    var navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+
+    ){
+        composable("login") {
+
+            Login(onIrPantalla2 = { navController.navigate("pantalla2")})
+
+        }
+        composable("pantalla2") {
+            Pantalla2()
+        }
+    }
+}
+
+
+
+@Composable
+fun Login(onIrPantalla2: () -> Unit) {
+    var nombre by remember { mutableStateOf("") }
+    var alias by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier.fillMaxSize()
             .padding(10.dp)
@@ -61,20 +108,20 @@ fun Login() {
         Spacer(modifier = Modifier.height(10.dp))
 
         OutlinedTextField(
-            value = "", //Valor vacío
-            onValueChange = {}, // aún sin nada al escribir
+            value = nombre,
+            onValueChange = { nombre = it},
             label = { Text("Nombre") },
-            singleLine = true, //una sola linea
+            singleLine = true,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
         )
         OutlinedTextField(
-            value = "", //Valor vacío
-            onValueChange = {}, // aún sin nada al escribir
+            value = alias,
+            onValueChange = { alias = it },
             label = {
                 Text("Alias")},
-            singleLine = true, //una sola linea
+            singleLine = true,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 10.dp, start = 16.dp, end = 16.dp)
@@ -82,27 +129,153 @@ fun Login() {
 
         Button(
             onClick = {
-                // 🔹 Creamos un intent para abrir la SecondActivity
-                val intent = Intent(context, SecondActivity::class.java)
-                context.startActivity(intent)
-            },
+                if (nombre.isNotBlank() && alias.isNotBlank()){
+                    globalNombre = nombre
+                    globalAlias = alias
+                    onIrPantalla2()
+                }
+                      },
             modifier = Modifier.fillMaxWidth()
-                .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                .padding(16.dp)
 
         ) {
-            Text(
-                "Continuar"
-            )
+            Text("Continuar")
         }
 
     }
 
 }
 
+
+
+@Composable
+fun Pantalla2() {
+
+    var nuevaTarea by remember { mutableStateOf("") }
+    val listaTareas = remember { mutableStateListOf<String>() }
+    var dropd by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+            .background(Color(0xFFE3F2FD)),
+
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 8.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+
+        ) {
+            Text(
+                "Hola, $globalNombre ($globalAlias)",
+                color = Color.Black
+            )
+
+            IconButton(onClick = { dropd = true }) {
+                Icon(
+                    imageVector = Icons.Filled.MoreVert,
+                    contentDescription = "Opciones",
+                    tint = Color.Black,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+            DropdownMenu(
+                expanded = dropd,
+                onDismissRequest = { dropd = false }
+            ) {
+                DropdownMenuItem(
+                    text = { Text("¿¿Ajustes??") },
+                    onClick = {
+                        dropd = false
+                    }
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+
+        ) {
+            OutlinedTextField(
+                value = nuevaTarea,
+                onValueChange = { nuevaTarea = it },
+                label = {
+                    Text("Nueva tarea")},
+                singleLine = true,
+                modifier = Modifier
+                    .weight(1f)
+            )
+
+
+
+            Button(
+                onClick = {
+                    if (nuevaTarea.isNotBlank()){
+                        listaTareas.add(nuevaTarea)
+                        nuevaTarea = ""
+                    }
+                },
+                modifier = Modifier.align(Alignment.CenterVertically)
+
+
+
+            ) {
+                Text(
+                    "Añadir"
+                )
+            }
+
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            listaTareas.forEach { nuevaTarea ->
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp),
+
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(nuevaTarea,
+                        modifier = Modifier.weight(1f))
+
+
+
+                    IconButton(onClick = { listaTareas.remove(nuevaTarea) }) {
+
+                        Icon(
+                            imageVector = Icons.Filled.RestoreFromTrash,
+                            contentDescription = "Eliminar",
+                            tint = Color.Black,
+
+                            modifier = Modifier
+                                .size(24.dp)
+                        ) } }
+                }
+        }
+
+
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TodoappTheme {
-        Login()
+        Pantalla2()
     }
 }
